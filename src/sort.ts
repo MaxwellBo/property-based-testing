90155ddcf2cd015d634c077221fcb26509a279ed
Max Bo <max@maxbo.me>
2019-09-30 11:00:34 +1000

    Update sort parameter naming

--- src/sort.ts
+++ src/sort.ts
@@ -1,25 +1,25 @@
 
-const sortInternal = <T>(tab: T[], start: number, end: number, cmp: (a: T, b: T) => boolean): T[] => {
-  if (end - start < 2) return tab;
+const sortInternal = <T>(xs: T[], start: number, end: number, cmp: (a: T, b: T) => boolean): T[] => {
+  if (end - start < 2) return xs;
 
   let pivot = start;
   for (let idx = start + 1; idx < end; ++idx) {
-    if (!cmp(tab[start], tab[idx])) {
-      let prev = tab[++pivot];
-      tab[pivot] = tab[idx];
-      tab[idx] = prev;
+    if (!cmp(xs[start], xs[idx])) {
+      let prev = xs[++pivot];
+      xs[pivot] = xs[idx];
+      xs[idx] = prev;
     }
   }
   
-  let prev = tab[pivot];
-  tab[pivot] = tab[start];
-  tab[start] = prev;
+  let prev = xs[pivot];
+  xs[pivot] = xs[start];
+  xs[start] = prev;
 
-  sortInternal(tab, start, pivot, cmp);
-  sortInternal(tab, pivot + 1, end, cmp);
-  return tab;
+  sortInternal(xs, start, pivot, cmp);
+  sortInternal(xs, pivot + 1, end, cmp);
+  return xs;
 };
 
-export const sort = <T>(tab: T[]): T[] => {
-  return sortInternal([...tab], 0, tab.length - 1, (a, b) => a < b);
+export const sort = <T>(xs: T[]): T[] => {
+  return sortInternal([...xs], 0, xs.length - 1, (a, b) => a < b);
 };
